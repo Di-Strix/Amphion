74d8e493479e84670f02aa4cf5b7261d0dea4ef0
Di-Strix <52571870+Di-Strix@users.noreply.github.com>
2026-03-30 21:44:34 +0200

    use `absolute` instead of `resolve` in `resolve_path` to keep symlinks

--- src/amphion/utils/path.py
+++ src/amphion/utils/path.py
@@ -5,4 +5,4 @@ from typing import Union
 def resolve_path(basepath: Union[Path, str], other: Union[Path, str] = "") -> Path:
     _basepath = Path(basepath).expanduser()
     _other = Path(other).expanduser()
-    return (_basepath / _other.expanduser()).resolve()
+    return (_basepath / _other.expanduser()).absolute()
